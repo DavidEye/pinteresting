fe6d4475d8b1842bc295edc121b94521c70ff782
David Eye <Davideye3@gmail.com>
2015-09-25 17:14:11 -0700

    Added SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "projects" => "pages#projects"
+  get "signup" => "pages#signup"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
